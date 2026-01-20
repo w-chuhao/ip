8f36c56557ef2ffae2b83a9780e2ae78a3429f78
w-chuhao <e1398566@u.nus.edu>
2026-01-21 00:13:24 +0800

    level 0

---
+++ src/main/java/Chu.java
@@ -0,0 +1,15 @@
+public class Chu {
+    public static void main(String[] args) {
+        String logo =
+                "  _____ _    _ _   _ \n"
+                        + " / ____| |  | | | | |\n"
+                        + "| |    | |__| | | | |\n"
+                        + "| |    |  __  | | | |\n"
+                        + "| |____| |  | | |_| |\n"
+                        + " \\_____|_|  |_|\\___/ \n";
+        System.out.println("Hello I'm\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
