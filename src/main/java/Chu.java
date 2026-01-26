6f9b59f6891ec51715d578cdee9c230976858c84
w-chuhao <e1398566@u.nus.edu>
2026-01-26 17:06:01 +0800

    Added storage and display of user input

--- src/main/java/Chu.java
+++ src/main/java/Chu.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Chu {
     public static void main(String[] args) {
@@ -13,9 +14,20 @@ public class Chu {
         System.out.println("What can I do for you?");
         String line;
         Scanner in = new Scanner(System.in);
+        String [] list = new String[100];
+        int counter = 0;
         line = in.nextLine();
         while(!(line.equalsIgnoreCase("bye"))){
-            System.out.println(line);
+            if(line.equalsIgnoreCase("list")){
+                for(int i=0;i<counter;i+=1){
+                    System.out.println((i+1) +". "+ list[i]);
+                }
+            }
+            else{
+                System.out.println("added " + line);
+                list[counter] = line;
+                counter+=1;
+            }
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
