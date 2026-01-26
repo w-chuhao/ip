3a0f226b9e7957ba6543ca34ccc6053ddce083ad
w-chuhao <e1398566@u.nus.edu>
2026-01-27 00:05:43 +0800

    Added mark, unmark function

--- src/main/java/Chu.java
+++ src/main/java/Chu.java
@@ -12,25 +12,53 @@ public class Chu {
                         + " \\_____|_|  |_|\\___/ \n";
         System.out.println("Hello I'm\n" + logo);
         System.out.println("What can I do for you?");
-        String line;
+
         Scanner in = new Scanner(System.in);
-        String [] list = new String[100];
+        Task[] list = new Task[100];
         int counter = 0;
-        line = in.nextLine();
-        while(!(line.equalsIgnoreCase("bye"))){
-            if(line.equalsIgnoreCase("list")){
-                for(int i=0;i<counter;i+=1){
-                    System.out.println((i+1) +". "+ list[i]);
+        int index = 0;
+
+        while (true) {
+            String line = in.nextLine().trim();
+
+            String[] sentence = line.split(" ");
+
+            switch (sentence[0].toLowerCase()) {
+            case "bye":
+                System.out.println("Bye. Hope to see you again soon!");
+                return;
+
+            case "list":
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < counter; i++) {
+                    System.out.println((i + 1) + ".[" + list[i].getStatusIcon() +"] "+ list[i].getDescription());
                 }
+                break;
+            case "mark":
+                if(sentence[1] != null){
+                    index = Integer.parseInt(sentence[1])-1;
+                }
+                list[index].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + list[index].getStatusIcon() +"] "+ list[index].getDescription());
+                break;
+
+            case "unmark":
+                if(sentence[1] != null){
+                    index = Integer.parseInt(sentence[1])-1;
+                }
+                list[index].unmark();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[" + list[index].getStatusIcon() +"] "+ list[index].getDescription());
+                break;
+
+            default:
+                System.out.println("added: " + line);
+                list[counter] = new Task(line);
+                counter += 1;
+                break;
             }
-            else{
-                System.out.println("added " + line);
-                list[counter] = line;
-                counter+=1;
-            }
-            line = in.nextLine();
         }
-        System.out.println("Bye. Hope to see you again soon!");
     }
 }
 
